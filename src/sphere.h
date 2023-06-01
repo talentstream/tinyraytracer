21f37b677bf12c49bf3e5dc0b6d27e4a0d56087b
talentstream <447721326@qq.com>
2023-06-01 10:39:00 +0800

    Revert "change the code structure"
    This reverts commit 33251309bfb95c18757701b7c235d5afacc742aa.

---
+++ src/sphere.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "hittable.h"
+
+class sphere : public hittable
+{
+public:
+    sphere() {}
+    sphere(point3 cen, double r) : center(cen), radius(r) {}
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override;
+    
+public:
+    point3 center;
+    double radius;
+};
